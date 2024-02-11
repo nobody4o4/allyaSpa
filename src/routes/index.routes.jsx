3c6681cf1684a2e87201b8e3d3fb8e8cff5c522c
Samir Gautam <96002032+nobody4o4@users.noreply.github.com>
2024-02-11 16:59:42 +0545

    first_

--- src/routes/index.routes.jsx
+++ src/routes/index.routes.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "../pages/Home";
+import Home from "../pages/Home.jsx";
 
 export default function MainRoute() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
