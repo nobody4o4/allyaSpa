2ca12e2b691f84fa02d759d690ed93902d793aaa
Gaurav <gurungg113@gmail.com>
2024-02-12 16:26:40 +0545

    reviews

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReviewsMain from '../features/reviewsMain'
+import MainSlider from '../components/slider/mainSlider'
 
 export default function Home() {
   return (
     <div className=''>
         <ReviewsMain />
+        <MainSlider />
+
     </div>
   )
 }
