e8201d0ee0314ea0f423e04ba72d22fd6388a8d2
Samir Gautam <96002032+nobody4o4@users.noreply.github.com>
2024-02-23 14:20:31 +0545

    navRes2

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,37 +6,36 @@ import Button from "./Button";
 
 function Gallery() {
   return (
-    <div className="flex flex-col h-[900 px]  mx-32 my-10  mt-[10rem]">
-      <div className="flex items-center justify-center mb-8">
-      <p className="text-5xl font-bold">Our Gallery</p>
-
+    <div className="h-[900 px] my-10 mt-[10rem]  flex flex-col md:mx-24 lg:mx-32">
+      <div className="mb-8 flex items-center justify-center">
+        <p className="text-5xl font-bold">Our Gallery</p>
       </div>
 
       <div>
-        <div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+        <div className="container mx-auto grid grid-cols-2 gap-4 p-4 md:grid-cols-4">
           <img
             src={image1}
             alt=""
-            className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 aspect-square object-cover"
+            className="col-span-2 row-span-2 aspect-square h-full min-h-96 w-full rounded object-cover shadow-sm md:col-start-3 md:row-start-1"
           />
           <img
             src={image2}
             alt=""
-            className="w-full h-full col-span-2 rounded  shadow-sm min-h-48 aspect-square object-cover"
+            className="col-span-2 aspect-square h-full min-h-48  w-full rounded object-cover shadow-sm"
           />
           <img
             src={image3}
             alt=""
-            className="w-full h-full rounded shadow-sm min-h-48 aspect-square object-cover"
+            className="aspect-square h-full min-h-48 w-full rounded object-cover shadow-sm"
           />
           <img
             src={image4}
             alt=""
-            className="w-full h-full  rounded shadow-sm min-h-48  aspect-square object-cover"
+            className="aspect-square h-full  min-h-48 w-full rounded  object-cover shadow-sm"
           />
         </div>
       </div>
-      <div className="flex mb-4 justify-center">
+      <div className="mb-4 flex justify-center">
         <Button title="View More" className="mx-auto" />
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,12 +5,10 @@ import AboutUs from "../features/home/aboutUs";
 import OurServices from "../features/home/ourServices";
 import ReviewsMain from "../features/home/reviewsMain";
 import Location from "../components/Location";
-import NavTest from "../components/NavTest";
 
 export default function Home() {
   return (
     <div className="">
-      <NavTest />
       <MainHeaders />
       <AboutUs />
       <OurServices />
