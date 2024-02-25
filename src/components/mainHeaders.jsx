d47426010b9acb1ff4c52e62e1a0de83c105209e
Samir Gautam <96002032+nobody4o4@users.noreply.github.com>
2024-02-25 12:13:50 +0545

    responsive3

--- src/components/mainHeaders.jsx
+++ src/components/mainHeaders.jsx
@@ -1,11 +1,11 @@
-import hero from "../assets/hero.png";
+import hero from "../assets/hero.jpg";
 
 export default function MainHeaders() {
   return (
     <div className="relative">
       <div className="absolute top-0 w-full"></div>
-      <div className="w-full h-screen">
-        <img className="w-full h-full object-cover" src={hero}></img>
+      <div className="h-screen w-full">
+        <img className="h-full w-full object-cover" src={hero}></img>
       </div>
     </div>
   );
